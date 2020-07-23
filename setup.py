adac84bd76239ff144bde0bcff892a3213bdb75a
Kajetan Bancerz <kajetan.bancerz@gmail.com>
2020-07-23 11:14:07 +0200

    Bump the version to 0.2.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "kimai-python"
-VERSION = "0.2.7"
+VERSION = "0.2.8"
 # To install the library, run the following
 #
 # python setup.py install
